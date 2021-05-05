64b91398535adedb0f05c1fc0c305fa5bf586af1
Italo931 <italojoaquin@hotmail.com>
2021-05-04 20:13:08 -0500

    RESERVASERVICEIMPL

---
+++ src/main/java/pe/fico/serviceimpl/IReservaServiceImpl.java
@@ -0,0 +1,34 @@
+package pe.fico.serviceimpl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import pe.fico.dao.IReservaDao;
+import pe.fico.entity.Reserva;
+import pe.fico.service.IReservaService;
+
+public class IReservaServiceImpl implements IReservaService, Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private IReservaDao mD;
+	
+	@Override
+	public void insertar(Reserva reserva) {
+		mD.insertar(reserva);
+	}
+
+	@Override
+	public List<Reserva> listar() {
+		return mD.listar();
+	}
+
+	@Override
+	public void eliminar(int idReserva) {
+		mD.eliminar(idReserva);
+		
+	}
+}
